3ad7182671cc68b67066184e485f7b3b1cee4e1d
iammrson <iammrson@hotmail.com>
2020-02-14 21:03:52 +0900

    2.5.6 completed

---
+++ demo1.py
@@ -0,0 +1,16 @@
+"""A demo1 is module."""
+
+def print_a():
+    """Prints a."""
+    print('a')
+
+def print_b():
+    """Prints b."""
+    print('b')
+    
+c = 2
+d = 2
+
+if __name__ == '__main__':
+    # print_a() is only executed when the module is run directly.
+    print_a()
